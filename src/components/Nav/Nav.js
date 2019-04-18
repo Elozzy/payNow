7c0865bd41dcfb981acc01c6ac796cdc6150fcb3
Elozzy <st.eloghosa@gmail.com>
2019-04-18 14:22:38 +0100

    made a few changes #not bug free

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 export default class Nav extends Component {
     render(){
